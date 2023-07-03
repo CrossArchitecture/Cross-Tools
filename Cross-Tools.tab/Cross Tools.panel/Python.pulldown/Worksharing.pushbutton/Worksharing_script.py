44e3d4f88a8db200dcf1b4104a32c266677399e0
CrossArchitecture <130647338+CrossArchitecture@users.noreply.github.com>
2023-07-03 15:25:34 +0200

    Update Worksharing_script.py

--- Cross-Tools.tab/Cross Tools.panel/Python.pulldown/Worksharing.pushbutton/Worksharing_script.py
+++ Cross-Tools.tab/Cross Tools.panel/Python.pulldown/Worksharing.pushbutton/Worksharing_script.py
@@ -3,26 +3,23 @@ from pyrevit import revit, DB
 def check_sync_status():
     doc = revit.doc
     sync_info = doc.GetWorksharingCentralModelInfo()
-
-    if sync_info.IsWorkshared and sync_info.IsModifiable:
-        # Das Modell wird synchronisiert
-        return True
-    else:
-        # Das Modell wird nicht synchronisiert
-        return False
-
-def sync_model():
-    # Führen Sie hier Ihren Synchronisationscode aus
-    doc = revit.doc
     current_user = doc.GetWorksharingCentralUser()
 
     if current_user:
         # Es gibt bereits einen Benutzer, der synchronisiert
-        print("Ein anderer Benutzer synchronisiert das Modell.")
+        return True
     else:
-        # Kein anderer Benutzer synchronisiert
-        # Führen Sie den Synchronisationscode aus
-        revit.uidoc.SynchronizeWithCentral(DB.TransactWithCentralOptions())
+        if sync_info.IsWorkshared and sync_info.IsModifiable:
+            # Das Modell wird synchronisiert
+            return False
+        else:
+            # Das Modell wird nicht synchronisiert
+            return False
+
+def sync_model():
+    # Führen Sie hier Ihren Synchronisationscode aus
+    doc = revit.doc
+    revit.uidoc.SynchronizeWithCentral(DB.TransactWithCentralOptions())
 
 # Überprüfen, ob das Modell synchronisiert wird
 if not check_sync_status():
