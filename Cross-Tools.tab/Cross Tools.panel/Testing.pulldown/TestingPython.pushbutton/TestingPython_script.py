4e0eaef1dea4af82790395250cdb3a75cf2ee5c7
CrossArchitecture <130647338+CrossArchitecture@users.noreply.github.com>
2023-07-27 11:21:42 +0200

    Update TestingPython_script.py

--- Cross-Tools.tab/Cross Tools.panel/Testing.pulldown/TestingPython.pushbutton/TestingPython_script.py
+++ Cross-Tools.tab/Cross Tools.panel/Testing.pulldown/TestingPython.pushbutton/TestingPython_script.py
@@ -1,4 +1,38 @@
-# ... (Previous code remains the same)
+# -*- coding: utf-8 -*-
+__title__   = "Center Room Tags"
+
+
+# ╦╔╦╗╔═╗╔═╗╦═╗╔╦╗╔═╗
+# ║║║║╠═╝║ ║╠╦╝ ║ ╚═╗
+# ╩╩ ╩╩  ╚═╝╩╚═ ╩ ╚═╝
+#==================================================
+from Autodesk.Revit.DB import *
+
+# ╦  ╦╔═╗╦═╗╦╔═╗╔╗ ╦  ╔═╗╔═╗
+# ╚╗╔╝╠═╣╠╦╝║╠═╣╠╩╗║  ║╣ ╚═╗
+#  ╚╝ ╩ ╩╩╚═╩╩ ╩╚═╝╩═╝╚═╝╚═╝
+#==================================================
+doc   = __revit__.ActiveUIDocument.Document
+uidoc = __revit__.ActiveUIDocument
+app   = __revit__.Application
+
+# ELEMENTS
+all_room_tags = FilteredElementCollector(doc, doc.ActiveView.Id)\
+    .OfCategory(BuiltInCategory.OST_RoomTags).WhereElementIsNotElementType().ToElements()
+
+# CONTROLS
+step = 2 # INTERNAL UNITS IN FEET
+
+
+def move_room_and_tag_to_upper_left(tag, room):
+    """Function to move both Room and Tag Locations to the upper-left corner of the room,
+    if they are not part of the group.
+    :param tag:     Room Tag
+    :param room:    Room"""
+    if room.GroupId == ElementId(-1):  # ElementId(-1) means None
+        room_bb = room.get_BoundingBox(doc.ActiveView)
+        room_upper_left = XYZ(room_bb.Min.X, room_bb.Max.Y, room_bb.Min.Z)
+        tag.Location.Point = room_upper_left
 
 # ╔╦╗╔═╗╦╔╗╔
 # ║║║╠═╣║║║║
@@ -9,35 +43,7 @@ with Transaction(doc, __title__) as t:
     t.Start()
 
     for tag in all_room_tags:
-        # ROOM DATA
         room = tag.Room
-        room_bb = room.get_BoundingBox(doc.ActiveView)
-        room_min = room_bb.Min
-
-        # MOVE TO UPPER-LEFT CORNER (if possible)
-        if room.IsPointInRoom(room_min):
-            move_room_and_tag(tag, room, room_min)
-
-        # FIND ANOTHER LOCATION
-        else:
-            room_boundaries = room.GetBoundarySegments(SpatialElementBoundaryOptions())
-            room_segments   = room_boundaries[0]
-
-            # Get Longest Segment
-            length = 0
-            longest_curve = None
-
-            for seg in room_segments:
-                curve = seg.GetCurve()
-                if curve.Length > length:
-                    longest_curve = curve
-                    length = curve.Length
-
-            # Get upper-left corner point of the Bounding Box
-            pt_upper_left = XYZ(room_bb.Min.X, room_bb.Max.Y, room_bb.Min.Z)
-
-            # Move the tag to the upper-left corner point if it is inside the room boundaries
-            if room.IsPointInRoom(pt_upper_left):
-                move_room_and_tag(tag, room, pt_upper_left)
+        move_room_and_tag_to_upper_left(tag, room)
 
     t.Commit()
