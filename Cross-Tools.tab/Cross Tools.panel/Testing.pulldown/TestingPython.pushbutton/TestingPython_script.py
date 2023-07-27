9332035ac0763a1dc58ab74499a51c61beb03255
CrossArchitecture <130647338+CrossArchitecture@users.noreply.github.com>
2023-07-27 11:26:19 +0200

    Update TestingPython_script.py

--- Cross-Tools.tab/Cross Tools.panel/Testing.pulldown/TestingPython.pushbutton/TestingPython_script.py
+++ Cross-Tools.tab/Cross Tools.panel/Testing.pulldown/TestingPython.pushbutton/TestingPython_script.py
@@ -24,15 +24,16 @@ all_room_tags = FilteredElementCollector(doc, doc.ActiveView.Id)\
 step = 2 # INTERNAL UNITS IN FEET
 
 
-def move_room_and_tag_to_upper_left(tag, room):
-    """Function to move both Room and Tag Locations to the upper-left corner of the room,
-    if they are not part of the group.
+def move_room_and_tag(tag, room, new_pt):
+    """Function to move both Room and Tag Locations, if they are not part of the group.
     :param tag:     Room Tag
-    :param room:    Room"""
+    :param room:    Room
+    :param new_pt:  XYZ Point."""
     if room.GroupId == ElementId(-1):  # ElementId(-1) means None
-        room_bb = room.get_BoundingBox(doc.ActiveView)
-        room_upper_left = XYZ(room_bb.Min.X, room_bb.Max.Y, room_bb.Min.Z)
-        tag.Location.Point = room_upper_left
+        room.Location.Point = new_pt
+
+    if tag.GroupId == ElementId(-1):
+        tag.Location.Point = new_pt
 
 # ╔╦╗╔═╗╦╔╗╔
 # ║║║╠═╣║║║║
@@ -44,6 +45,8 @@ with Transaction(doc, __title__) as t:
 
     for tag in all_room_tags:
         room = tag.Room
-        move_room_and_tag_to_upper_left(tag, room)
+        room_bb = room.get_BoundingBox(doc.ActiveView)
+        room_upper_left = XYZ(room_bb.Min.X, room_bb.Max.Y, room_bb.Min.Z)
+        move_room_and_tag(tag, room, room_upper_left)
 
     t.Commit()
